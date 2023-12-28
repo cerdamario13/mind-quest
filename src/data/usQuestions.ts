df06ff11588182d9a9d53f7ffe175191d0b88276
Mario <cerdamario13@gmail.com>
2023-12-27 21:45:53 -0600

    question temp db

---
+++ src/data/usQuestions.ts
@@ -0,0 +1,44 @@
+export const usHistoryQuestions = [
+  {
+    key: 1,
+    enQuestion: "Who was the first president of the United States?",
+    esQuestion: "¿Quién fue el primer presidente de los Estados Unidos?",
+    answer: "George Washington",
+    choices: ["George Washington", "Abraham Lincoln", "Thomas Jefferson", "John Adams"]
+  },
+  {
+    key: 2,
+    enQuestion: "What is the capital of the United States?",
+    esQuestion: "¿Cuál es la capital de los Estados Unidos?",
+    answer: "Washington, D.C.",
+    choices: ["Washington, D.C.", "New York City", "Los Angeles", "Chicago"]
+  },
+  {
+    key: 3,
+    enQuestion: "What is the largest state in the United States?",
+    esQuestion: "¿Cuál es el estado más grande de los Estados Unidos?",
+    answer: "Alaska",
+    choices: ["Alaska", "Texas", "California", "Montana"]
+  },
+  {
+    key: 4,
+    enQuestion: "What is the smallest state in the United States?",
+    esQuestion: "¿Cuál es el estado más pequeño de los Estados Unidos?",
+    answer: "Rhode Island",
+    choices: ["Rhode Island", "Delaware", "Connecticut", "New Jersey"]
+  },
+  {
+    key: 5,
+    enQuestion: "What is the longest river in the United States?",
+    esQuestion: "¿Cuál es el río más largo de los Estados Unidos?",
+    answer: "Missouri River",
+    choices: ["Missouri River", "Mississippi River", "Colorado River", "Rio Grande"]
+  },
+  {
+    key: 6,
+    enQuestion: "Who was the second president of the United States?",
+    esQuestion: "¿Quién fue el segundo presidente de los Estados Unidos?",
+    answer: "John Adams",
+    choices: ["John Adams", "George Washington", "Thomas Jefferson", "James Madison"]
+  }
+];
