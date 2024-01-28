7dfa61c194d5976d59b57c7a544514d32835f8ee
Mario <cerdamario13@gmail.com>
2024-01-27 19:23:59 -0600

    Added working random questions

--- src/Question/questionGeneration.ts
+++ src/Question/questionGeneration.ts
@@ -3,12 +3,28 @@ var questionDataStorage = JSON.parse(localStorage.getItem('projects/mindQuest/qu
 
 export const questionGeneration = (category: string) => {
   //get the questions using the category from the data
-  var dataArray = questionDataStorage.filter((obj: questionDataProps) => obj.sheetName === category);
-  var questions = dataArray[0].questions;
   
-  //randomly select one question from the questions array
-  const random = Math.floor(Math.random() * questions.length);
-  return questions[random];
+  if (category == 'random') {
+    //Get all of the questions from the data and choose one at random
+    var allQuestions: any[] = [];
+    questionDataStorage.map((quest: any) => {
+      allQuestions.push(...quest.questions);
+    });
+    
+    //randomly select one question from the questions array
+    const random = Math.floor(Math.random() * allQuestions.length);
+    return allQuestions[random];
+  } else {
+    
+    var dataArray = questionDataStorage.filter((obj: questionDataProps) => obj.sheetName === category);
+    var questions = dataArray[0].questions;
+    
+    //randomly select one question from the questions array
+    const random = Math.floor(Math.random() * questions.length);
+    return questions[random];
+    
+  }
+  
 };
 
 interface questionDataProps {
